22ae9c38ac65e35808b1b3f722c6f2ad55760138
lifestand <33244968+lifestand@users.noreply.github.com>
2017-10-31 23:30:34 -0600

    Image (Designer Team issue)
    Image added to the drawable resource, and implemented in the MainActivity layout. (ps: the image was enormous, at can only be rize to 118pix x 118px if the image needs to be rize even smaller we will need a new version of the logo, only this time smaller) [Editor note: if the image is going to be used as a logo do not make it the size of a full letter size page]

--- Proyecto_Arquitectura/app/src/main/java/mx/itesm/maldi/proyecto_arquitectura/MainActivity.java
+++ Proyecto_Arquitectura/app/src/main/java/mx/itesm/maldi/proyecto_arquitectura/MainActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 
@@ -17,6 +18,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        ImageView img= (ImageView) findViewById(R.id.imageView);
+        img.setImageResource(R.drawable.imagen1);
         Handler handler = new Handler();
         handler.postDelayed(new Runnable(){
             @Override
