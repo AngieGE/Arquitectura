548a37c1c8cddd636736b1bb6b2c96bb5e5f101f
lifestand <33244968+lifestand@users.noreply.github.com>
2017-11-01 22:29:46 -0600

    Enter Button in password layout
    Password submit button added code still pending

--- Proyecto_Arquitectura/app/src/main/java/mx/itesm/maldi/proyecto_arquitectura/Password.java
+++ Proyecto_Arquitectura/app/src/main/java/mx/itesm/maldi/proyecto_arquitectura/Password.java
@@ -2,17 +2,24 @@ package mx.itesm.maldi.proyecto_arquitectura;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.EditText;
+import android.widget.ImageView;
 
 /**
  * Created by maldi on 31/10/2017.
  */
 
 public class Password extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.password);
     }
+
+    //EditText pass = (EditText) findViewById(R.id.password);
+    //String pass_content = pass.getText().toString();
+
 
 
 }
