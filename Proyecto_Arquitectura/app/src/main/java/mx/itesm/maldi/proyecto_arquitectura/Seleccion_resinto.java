345b326407faa067c80f20209d31b0a722559348
lifestand <33244968+lifestand@users.noreply.github.com>
2017-11-04 12:47:32 -0600

    Compund selection code done
    Codification of the button selection of the compund issues with logos size and format. MUST BE TALK ABOUT

--- Proyecto_Arquitectura/app/src/main/java/mx/itesm/maldi/proyecto_arquitectura/Seleccion_resinto.java
+++ Proyecto_Arquitectura/app/src/main/java/mx/itesm/maldi/proyecto_arquitectura/Seleccion_resinto.java
@@ -1,13 +1,34 @@
 package mx.itesm.maldi.proyecto_arquitectura;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.Toast;
 
-public class Seleccion_resinto extends AppCompatActivity {
+public class Seleccion_resinto extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_seleccion_resinto);
+        ImageButton avenda= (ImageButton) findViewById(R.id.Avenda);
+        avenda.setImageResource(R.drawable.avenda);
+        avenda.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.enter: //Enter
+                Intent i;
+                i = new Intent(Seleccion_resinto.this, Seleccion_resinto.class);
+                startActivity(i);
+               break;
+        }
+
     }
 }
