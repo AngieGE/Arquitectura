f3287c76422e6317f32a5039154ed27e99efb345
lifestand <33244968+lifestand@users.noreply.github.com>
2017-11-05 16:35:43 -0600

    Codificacion del layout seleccion de torre terminada

--- Proyecto_Arquitectura/app/src/main/java/mx/itesm/maldi/proyecto_arquitectura/Selecion_Torre.java
+++ Proyecto_Arquitectura/app/src/main/java/mx/itesm/maldi/proyecto_arquitectura/Selecion_Torre.java
@@ -1,13 +1,37 @@
 package mx.itesm.maldi.proyecto_arquitectura;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
-public class Selecion_Torre extends AppCompatActivity {
+public class Selecion_Torre extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_selecion__torre);
+        ImageButton torre_basica= (ImageButton) findViewById(R.id.basic_apartment);
+        torre_basica.setImageResource(R.drawable.basic_department_averanda);
+        torre_basica.setOnClickListener(this);
+        ImageButton torre_penthouse= (ImageButton) findViewById(R.id.penthouse_appartment);
+        torre_penthouse.setImageResource(R.drawable.penthouse_department_averanda);
+        torre_penthouse.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        Intent i;
+        switch (v.getId()) {
+            case R.id.basic_apartment: //Enter
+                //i = new Intent(Selecion_Torre.this, Avenda_Screen_Load.class);
+                //startActivity(i);
+                break;
+            case R.id.penthouse_appartment: //Enter
+                //i = new Intent(Selecion_Torre.this, Avenda_Screen_Load.class);
+                //startActivity(i);
+                break;
+        }
     }
 }
