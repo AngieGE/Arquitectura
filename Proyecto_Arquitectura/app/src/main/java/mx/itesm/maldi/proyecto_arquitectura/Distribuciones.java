ea39f8a29f3fd4c0897a93a5ed6d07312bcce262
lifestand <33244968+lifestand@users.noreply.github.com>
2017-11-09 21:10:18 -0600

    Custom layout alert
    Creacion de el custom_alert_layout que sera el alert de la seleccion de distribuciones, codificacion e implementacion en la activity de distribucion completada

--- Proyecto_Arquitectura/app/src/main/java/mx/itesm/maldi/proyecto_arquitectura/Distribuciones.java
+++ Proyecto_Arquitectura/app/src/main/java/mx/itesm/maldi/proyecto_arquitectura/Distribuciones.java
@@ -1,11 +1,15 @@
 package mx.itesm.maldi.proyecto_arquitectura;
 
+import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 
-public class Distribuciones extends AppCompatActivity {
+public class Distribuciones extends AppCompatActivity implements View.OnClickListener  {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,7 +21,25 @@ public class Distribuciones extends AppCompatActivity {
         dis1.setImageResource(R.drawable.distribucion1);
         dis2.setImageResource(R.drawable.distribucion2);
         dis3.setImageResource(R.drawable.distribucion3);
+        dis1.setOnClickListener(this);
 
+
+    }
+
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.distribucion1: //Enter
+                Custom_alert_layout cdd = new Custom_alert_layout(Distribuciones.this);
+                cdd.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+                cdd.show();
+                break;
+            case R.id.distribucion2: //Enter
+
+                break;
+            case R.id.distribucion3: //Enter
+
+                break;
+        }
 
     }
 }
