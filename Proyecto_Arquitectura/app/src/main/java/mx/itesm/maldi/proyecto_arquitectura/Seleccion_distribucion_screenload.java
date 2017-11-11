63ce7c80dac6de96ba55e1351fd842c860efa975
lifestand <33244968+lifestand@users.noreply.github.com>
2017-11-11 16:50:59 -0600

    No se que hice

--- Proyecto_Arquitectura/app/src/main/java/mx/itesm/maldi/proyecto_arquitectura/Seleccion_distribucion_screenload.java
+++ Proyecto_Arquitectura/app/src/main/java/mx/itesm/maldi/proyecto_arquitectura/Seleccion_distribucion_screenload.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Window;
+import android.widget.ImageView;
 
 public class Seleccion_distribucion_screenload extends AppCompatActivity {
 
@@ -11,12 +13,18 @@ public class Seleccion_distribucion_screenload extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_seleccion_distribucion_screenload);
+        Intent intent = getIntent();
+        int temp = intent.getIntExtra("int_value", 0);
+        ImageView img= (ImageView) findViewById(R.id.imagen_dist);
+        if (temp != 0) {
+            img.setImageResource(temp);
+        }
         Handler handler = new Handler();
         handler.postDelayed(new Runnable(){
             @Override
             public void run(){
                 Intent i;
-                i = new Intent(Seleccion_distribucion_screenload.this, Distribuciones.class);
+                i = new Intent(Seleccion_distribucion_screenload.this, Estilos.class);
                 startActivity(i);
                 overridePendingTransition(R.anim.fade_in, R.anim.fade_out);
             }
