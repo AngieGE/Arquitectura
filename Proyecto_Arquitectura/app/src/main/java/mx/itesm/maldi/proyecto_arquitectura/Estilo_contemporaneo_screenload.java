9d60cc89ae4a8812700c1689a4445f1b53d35fa3
lifestand <33244968+lifestand@users.noreply.github.com>
2017-11-12 13:32:56 -0600

    Layout de Stilos screen load
    codificacion de layout estilos screen load terminado

---
+++ Proyecto_Arquitectura/app/src/main/java/mx/itesm/maldi/proyecto_arquitectura/Estilo_contemporaneo_screenload.java
@@ -0,0 +1,27 @@
+package mx.itesm.maldi.proyecto_arquitectura;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Estilo_contemporaneo_screenload extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_estilo_contemporaneo_screenload);
+
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable(){
+            @Override
+            public void run(){
+                Intent i;
+                i = new Intent(Estilo_contemporaneo_screenload.this, Estilos.class);
+                startActivity(i);
+                overridePendingTransition(R.anim.fade_in, R.anim.fade_out);
+            }
+        }, 3000);
+    }
+
+}
