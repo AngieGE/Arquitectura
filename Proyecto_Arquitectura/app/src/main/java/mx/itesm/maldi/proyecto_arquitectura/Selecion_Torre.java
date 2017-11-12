9e9793d6d7f8f2907ac4a9eac902871cf05d229f
AngieGE <30639215+AngieGE@users.noreply.github.com>
2017-11-11 22:02:04 -0600

    Ayuda con Seleccion Torre

--- Proyecto_Arquitectura/app/src/main/java/mx/itesm/maldi/proyecto_arquitectura/Selecion_Torre.java
+++ Proyecto_Arquitectura/app/src/main/java/mx/itesm/maldi/proyecto_arquitectura/Selecion_Torre.java
@@ -8,11 +8,11 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.RelativeLayout;
 
 public class Selecion_Torre extends AppCompatActivity implements View.OnClickListener {
 
-    private Button torreA, torreB, torreC;
-    boolean tmp = false;
+    private Button torreA, torreB, torreC, sig, atras;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,31 +21,49 @@ public class Selecion_Torre extends AppCompatActivity implements View.OnClickLis
         torreA=(Button)findViewById(R.id.bn_a);
         torreB=(Button)findViewById(R.id.bn_b);
         torreC=(Button)findViewById(R.id.bn_c);
+        sig=(Button) findViewById(R.id.bn_siguiente_dis);
+        atras=(Button) findViewById(R.id.bn_atras_dis);
+
 
         torreA.setOnClickListener(this);
         torreB.setOnClickListener(this);
         torreC.setOnClickListener(this);
+        sig.setOnClickListener(this);
+        atras.setOnClickListener(this);
 
+        //Poner la imagen torre_arreglada de fondo en el relative layout
     }
 
     @Override
     public void onClick(View v) {
         Intent i;
-        tmp = !tmp;
         switch (v.getId()) {
-            case R.id.bn_a: //Enter
+            case R.id.bn_a: //Seleccionó torre a
+                //Cambiar el fondo del relative layout a img_torre_a
                 i = new Intent(Selecion_Torre.this, seleccion_tipo_departamento.class);
-                v.setBackgroundColor(tmp ? Color.RED : Color.argb(0,223,255,10));  //cambiar color
                 startActivity(i);
                 break;
-            case R.id.bn_b: //Enter
+            case R.id.bn_b: //Seleccionó torre b
+                //Cambiar el fondo del relative layout a img_torre_b
                 i = new Intent(Selecion_Torre.this, seleccion_tipo_departamento.class);
-                v.setBackgroundColor(tmp ? Color.RED : Color.argb(0,223,255,10));  //cambiar color
                 startActivity(i);
                 break;
-            case R.id.bn_c: //Enter
+            case R.id.bn_c: //Seleccionó torre c
+                //Cambiar el fondo del relative layout a img_torre_c
                 i = new Intent(Selecion_Torre.this, seleccion_tipo_departamento.class);
-                v.setBackgroundColor(tmp ? Color.RED : Color.argb(0,223,255,10)); //cambiar color
+                startActivity(i);
+                break;
+            case R.id.bn_atras_dis: //Seleccionó atras
+                i = new Intent(Selecion_Torre.this, Seleccion_resinto.class);
+                startActivity(i);
+                break;
+            case R.id.bn_siguiente_dis: //Seleccionó siguiente
+                i = new Intent(Selecion_Torre.this, seleccion_tipo_departamento.class);
+                //if (fondo del layout NO es "torre_arreglada"){
+                // startActivity(i)
+                // }else{
+                // Toast.makeText(getApplication(),"Seleccione una torre", Toast.LENGTH_SHORT).show();
+                // }
                 startActivity(i);
                 break;
         }
