5731eb18ca3c0b2aa31b82bbca7a333cbb31c50a
AngieGE <30639215+AngieGE@users.noreply.github.com>
2017-11-13 19:18:06 -0600

    Avance en clase e interfaz Catalogo

--- Proyecto_Arquitectura/app/src/main/java/mx/itesm/maldi/proyecto_arquitectura/Catalogo.java
+++ Proyecto_Arquitectura/app/src/main/java/mx/itesm/maldi/proyecto_arquitectura/Catalogo.java
@@ -11,25 +11,28 @@ import android.widget.Toast;
 
 public class Catalogo extends AppCompatActivity implements View.OnClickListener  {
 
-    private CheckBox articulo1, articulo2, articulo3;
+    private CheckBox cbSillon1, cbSillon2, cbSillon3, cbSillon4, cbSillon5, cbSillon6 ;
     private ImageView imagen;
-    private Button agrega;
+    private Button atras, siguiente;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_catalogo);
         //Declatro las check box y el boton
-        articulo1=(CheckBox)findViewById(R.id.check1);
-        articulo2=(CheckBox)findViewById(R.id.check2);
-        articulo3=(CheckBox)findViewById(R.id.check3);
+        cbSillon1=(CheckBox)findViewById(R.id.check1);
+        cbSillon2=(CheckBox)findViewById(R.id.check2);
+        cbSillon3=(CheckBox)findViewById(R.id.check3);
+
         imagen=(ImageView) findViewById(R.id.carrito_seleccion);
-        agrega=(Button)findViewById(R.id.agregar);
+        atras=(Button) findViewById(R.id.bn_atras_catalogo);
+        siguiente=(Button) findViewById(R.id.bn_siguiente_catalogo);
 
         //Les agrego su OnClickListener
-        articulo1.setOnClickListener(this);
-        articulo2.setOnClickListener(this);
-        articulo3.setOnClickListener(this);
-        agrega.setOnClickListener(this);
+        cbSillon1.setOnClickListener(this);
+        cbSillon2.setOnClickListener(this);
+        cbSillon3.setOnClickListener(this);
+        atras.setOnClickListener(this);
+        siguiente.setOnClickListener(this);
     }
 
     @Override
@@ -37,40 +40,40 @@ public class Catalogo extends AppCompatActivity implements View.OnClickListener 
         Intent i;
         switch (v.getId()) {
             case R.id.check1: //Deseleccionar la B en caso de haberla seleccionado antes
-                if(articulo2.isChecked()==true){
-                    articulo2.setChecked(false);
+                if(cbSillon2.isChecked()==true){
+                    cbSillon2.setChecked(false);
                 }
-                if(articulo3.isChecked()==true){
-                    articulo3.setChecked(false);
+                if(cbSillon3.isChecked()==true){
+                    cbSillon3.setChecked(false);
                 }
                 break;
             case R.id.check2: //Deseleccionar la A en caso de haberla seleccionado antes
-                if(articulo1.isChecked()==true){
-                    articulo1.setChecked(false);
+                if(cbSillon1.isChecked()==true){
+                    cbSillon1.setChecked(false);
                 }
-                if(articulo3.isChecked()==true){
-                    articulo3.setChecked(false);
+                if(cbSillon3.isChecked()==true){
+                    cbSillon3.setChecked(false);
                 }
                 break;
             case R.id.check3: //Deseleccionar la A en caso de haberla seleccionado antes
-                if(articulo1.isChecked()==true){
-                    articulo1.setChecked(false);
+                if(cbSillon1.isChecked()==true){
+                    cbSillon1.setChecked(false);
                 }
-                if(articulo2.isChecked()==true){
-                    articulo2.setChecked(false);
+                if(cbSillon2.isChecked()==true){
+                    cbSillon2.setChecked(false);
                 }
                 break;
-            case R.id.agregar:
-                if(articulo1.isChecked()==false && articulo2.isChecked()==false && articulo3.isChecked()==false) //en caso de no haber seleccionado alguna
+            case R.id.bn_siguiente_catalogo:
+                if(cbSillon1.isChecked()==false && cbSillon2.isChecked()==false && cbSillon3.isChecked()==false) //en caso de no haber seleccionado alguna
                 {
                     Toast.makeText(this, "Seleccione un articulo a agregar", Toast.LENGTH_SHORT).show();
-                }else if(articulo1.isChecked())//En caso de haber seleccionado opcion Tipo A
+                }else if(cbSillon1.isChecked())//En caso de haber seleccionado opcion Tipo A
                 {
                     imagen.setImageResource(R.drawable.imagen1);
-                }else if(articulo2.isChecked())//En caso de haber seleccionado opcion Tipo B
+                }else if(cbSillon2.isChecked())//En caso de haber seleccionado opcion Tipo B
                 {
                     imagen.setImageResource(R.drawable.zoom2);
-                }else if(articulo3.isChecked())//En caso de haber seleccionado opcion Tipo A
+                }else if(cbSillon3.isChecked())//En caso de haber seleccionado opcion Tipo A
                 {
 
                 }
